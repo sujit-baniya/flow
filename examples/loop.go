0e42a976eb11c328e64cb785d36b7c4574f2e2c6
Sujit <itsursujit@gmail.com>
2021-12-26 20:54:37 +0545

    Add README.md

--- examples/loop.go
+++ examples/loop.go
@@ -3,40 +3,43 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/sujit-baniya/flow"
+	"strings"
 )
 
+func GetSentence(ctx context.Context, d flow.Data) (flow.Data, error) {
+	words := strings.Split(d.ToString(), ` `)
+	bt, _ := json.Marshal(words)
+	d.Payload = bt
+	return d, nil
+}
+
+func ForEachWord(ctx context.Context, d flow.Data) (flow.Data, error) {
+	return d, nil
+}
+
+func WordUpperCase(ctx context.Context, d flow.Data) (flow.Data, error) {
+	d.Payload = flow.Payload(strings.ToTitle(d.ToString()))
+	fmt.Println(d.ToString())
+	return d, nil
+}
+
+func wordNodes() {
+	flow.AddNode("get-sentence", GetSentence)
+	flow.AddNode("for-each-word", ForEachWord)
+	flow.AddNode("upper-case", WordUpperCase)
+}
+
 func main() {
-	messages := []string{"this", "is", "a", "test"}
-	bt, _ := json.Marshal(messages)
-	flow.AddNode("for-each-message", ForEachMessage)
-	flow.AddNode("get-messages", GetMessages)
-	flow.AddNode("message", Message)
-	flow.AddNode("send", Send)
+	wordNodes()
 	flow1 := flow.New()
-	flow1.Loop("for-each-message", "message")
-	flow1.Edge("get-messages", "for-each-message")
-	flow1.Edge("message", "send")
+	flow1.Loop("for-each-word", "upper-case")
+	flow1.Edge("get-sentence", "for-each-word")
 	_, e := flow1.Build().Process(context.Background(), flow.Data{
-		Payload: bt,
+		Payload: flow.Payload("this is a sentence"),
 	})
 	if e != nil {
 		panic(e)
 	}
-}
-
-func GetMessages(ctx context.Context, d flow.Data) (flow.Data, error) {
-	return d, nil
-}
-
-func Message(ctx context.Context, d flow.Data) (flow.Data, error) {
-	return d, nil
-}
-
-func Send(ctx context.Context, d flow.Data) (flow.Data, error) {
-	return d, nil
-}
-
-func ForEachMessage(ctx context.Context, d flow.Data) (flow.Data, error) {
-	return d, nil
 }
